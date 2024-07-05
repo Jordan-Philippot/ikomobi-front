28af2e305c9ced65771d1823dcf9097e2b8ee461
Jordan-Philippot <jordan.philippot@epitech.eu>
2024-07-05 15:31:30 +0200

    Fix Messages Component

--- src/app/main/Messages.tsx
+++ src/app/main/Messages.tsx
@@ -11,22 +11,26 @@ import Toaster from "@/app/ui/Toaster";
 function Messages() {
   const { messages } = useSelector((state: RootState) => state.message);
 
-  return ReactDOM.createPortal(
-    <div className="messages-container">
-      {messages.map((message) => (
-        <div className="__toaster" key={message.date}>
-          <Toaster
-            message={message.text}
-            status={message.status}
-            onClick={message.onClick}
-            duration={5000}
-            style={{ position: "relative" }}
-          />
-        </div>
-      ))}
-    </div>,
-    document.body
-  );
+  if (typeof window === "object") {
+    return ReactDOM.createPortal(
+      <div className="messages-container">
+        {messages.map((message) => (
+          <div className="__toaster" key={message.date}>
+            <Toaster
+              message={message.text}
+              status={message.status}
+              onClick={message.onClick}
+              duration={5000}
+              style={{ position: "relative" }}
+            />
+          </div>
+        ))}
+      </div>,
+      document.body
+    );
+  }
+
+  return null;
 }
 
 export default Messages;
